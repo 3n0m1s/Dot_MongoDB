36bde508e4107e27012b4c6f5b468b3df088c033
Simone Schiavolini <simone.schiavolini@progettispeciali.net>
2018-09-27 13:34:26 +0200

    Add files via upload

---
+++ MongoDBFUll/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MongoDBFUll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtActive = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(744, 260);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Aggiungi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(32, 305);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(182, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(32, 337);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(182, 20);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // txtActive
+            // 
+            this.txtActive.Location = new System.Drawing.Point(32, 374);
+            this.txtActive.Name = "txtActive";
+            this.txtActive.Size = new System.Drawing.Size(182, 20);
+            this.txtActive.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtActive);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtActive;
+    }
+}
+
---
+++ MongoDBFUll/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace MongoDBFUll
+{
+
+    class Person
+    {
+        public ObjectId _id { get; set; }
+        public string Name { get; set; }
+        public string Phones { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+            MongoClientSettings user = new MongoClientSettings()
+            {
+                Server = new MongoServerAddress("www.epocum.com", 27017),
+                Credentials = new MongoCredential[] { MongoCredential.CreateCredential("admin", "mimonenew", "mimonenewpass") }
+            };
+
+            var client = new MongoClient(user);
+            var database = client.GetDatabase("admin");
+            var collection = database.GetCollection<BsonDocument>("persone");
+
+            var list = await collection.Find(new BsonDocument()).ToListAsync();
+
+            dataGridView1.Columns.Add("name", "Nome");
+            dataGridView1.Columns.Add("tel", "Telefono");
+            dataGridView1.Columns.Add("att", "Attivo");
+
+            foreach (var p in list)
+            {
+                dataGridView1.Rows.Add(p[1], p[2], p[3]);
+            }
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            MongoClientSettings user = new MongoClientSettings()
+            {
+                Server = new MongoServerAddress("www.epocum.com", 27017),
+                Credentials = new MongoCredential[] { MongoCredential.CreateCredential("admin", "mimonenew", "mimonenewpass") }
+            };
+
+            var client = new MongoClient(user);
+            var database = client.GetDatabase("admin");
+            var collection = database.GetCollection<Person>("persone");
+
+            var persona = new Person { Name = txtName.Text, Phones =  txtPhone.Text, IsActive = true};
+            await collection.InsertOneAsync(persona);
+
+            var collection2 = database.GetCollection<BsonDocument>("persone");
+            var list = await collection2.Find(new BsonDocument()).ToListAsync();
+
+            createView();
+            foreach (var p in list)
+            {
+                dataGridView1.Rows.Add(p[1], p[2], p[3]);
+            }
+            dataGridView1.Refresh();
+        }
+
+        void createView()
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("name", "Nome");
+            dataGridView1.Columns.Add("tel", "Telefono");
+            dataGridView1.Columns.Add("att", "Attivo");
+            dataGridView1.Rows.Clear();
+        }
+
+        private void DB_create_Collection()
+        {
+
+            MongoClientSettings user = new MongoClientSettings()
+            {
+                Server = new MongoServerAddress("www.epocum.com", 27017),
+                Credentials = new MongoCredential[] { MongoCredential.CreateCredential("admin", "mimonenew", "mimonenewpass") }
+            };
+
+            var client = new MongoClient(user);
+            var database = client.GetDatabase("admin");
+            database.CreateCollection("test");
+            Console.WriteLine("Create!");
+        }
+    }
+}
